6a4626e7ebd1d046962d31b62428a9b3762b3f2f
Fabrício Antõnio <fabricio.ss2117@gmail.com>
2025-07-06 19:26:45 -0300

    :sparkles: feat: list folder and open in VS Code

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const { resolve, basename } = require("path");
-const { app, Tray, Menu, dialog } = require("electron");
+const { app, Tray, Menu, MenuItem, dialog } = require("electron");
 const Store = require("electron-store");
+const { spawn, spawnSync } = require("child_process");
 
 const schema = {
   projects: {
@@ -9,7 +10,7 @@ const schema = {
   },
 };
 
-if (app.dock) {    // Only to macOS
+if (app.dock) { // Only to macOS
   app.dock.hide(); // Hide icon of Dock
 }
 
@@ -21,27 +22,41 @@ app.on("ready", () => {
   const tray = new Tray(resolve(process.cwd(), "assets", iconFile));
   const projects = store.get("projects", []);
 
-  console.log(projects);
+  const items = projects.map((project) => {
+    return {
+      label: project.name,
+      click: () => spawnSync("code", [project.path]),
+    };
+  });
 
   const contextMenu = Menu.buildFromTemplate([
+    ...items,
     {
-      label: "Item1",
-      type: "radio",
-      checked: true,
+      type: "separator",
+    },
+  ]);
+  
+  contextMenu.insert(0, new MenuItem(
+    {
+      label: "Add new project...",
       click: () => {
         const [path] = dialog.showOpenDialogSync({
           properties: ["openDirectory"],
         });
+        const name = basename(path);
         store.set("projects", [
           ...projects,
           {
             path,
-            name: basename(path),
+            name,
           },
         ]);
+        const item = new MenuItem({ label: name, click: () => spawnSync("code", [path]) });
+        contextMenu.append(item)
+        tray.setContextMenu(contextMenu);
       },
-    },
-  ]);
+    }
+  ))
 
   tray.setToolTip("TrayOpen — status: on 🔥");
   tray.setContextMenu(contextMenu);
